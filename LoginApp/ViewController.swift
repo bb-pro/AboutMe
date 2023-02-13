5b50980f25cbdbf4c34ca1c8448080e708670cce
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-02-13 16:04:10 +0500

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Bektemur Mamashayev on 13/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
